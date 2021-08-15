2aa619af9a309b6ca49774c4f97f8058da3cc7b5
Willem Jacobs <wamj58@gmail.com>
2021-08-14 19:09:05 -0500

    final API

--- server.js
+++ server.js
@@ -2,21 +2,18 @@
 
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const jwksClient = require("jwks-rsa");
-
 const app = express();
+
+const cors = require("cors");
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
-
-var jwksClient = require("jwks-rsa");
-const { response } = require("express");
-var client = jwksClient({
-  // EXCEPTION!  jwksUri comes from your single page application -> settings -> advanced settings -> endpoint -> the jwks one
+const jwt = require("jsonwebtoken");
+const jwksClient = require("jwks-rsa");
+const client = jwksClient({
   jwksUri: "https://dev-bqwezv2c.us.auth0.com/.well-known/jwks.json",
 });
+
+const PORT = process.env.PORT || 3001;
 
 function getKey(header, callback) {
   client.getSigningKey(header.kid, function (err, key) {
@@ -37,12 +34,6 @@ app.get("/test", (req, res) => {
     }
     res.send(user);
   });
-
-  // TODO:
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
